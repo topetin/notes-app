3b793d35d950bbc0d5a6e35a1f12d4fb374b3db8
topetin <carballalmariana@gmail.com>
2019-03-19 18:02:59 -0300

    add and delete notes with json storage

--- notes.js
+++ notes.js
@@ -1,6 +1,60 @@
+const fs = require('fs');
 
 const getNotes = function() {
     return 'Your notes...';
 }
 
-module.exports = getNotes;
+const addNote = function (title, body) {
+    const notes = loadNotes();
+    const duplicateNotes = notes.filter(function (note){
+        return note.title === title;
+    });
+
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        });
+        console.log('New note added.');
+        
+    } else {
+        console.log('Note duplicated, not added.');
+    }
+
+    saveNotes(notes);
+}
+
+const removeNote = function(title) {
+    const notes = loadNotes();
+    const noteToDelete = notes.find(note => note.title === title);
+
+    if (noteToDelete != null) {
+        saveNotes(notes.filter(note => note.title !== title));
+        console.log('Note deleted');
+        
+    } else {
+        console.log('Note not found.');
+    }
+}
+
+const saveNotes = function(notes) {
+    const dataJson = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', dataJson);
+}
+
+const loadNotes = function() {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJson = dataBuffer.toString()
+        return JSON.parse(dataJson);
+    }
+    catch (e) {
+        return [];
+    }
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote
+}
